5961ff481fd4b1245a8fc508b00c316b3dea8c6c
SPIRIDONOV ANDREW <spiridonov.andrew.2.0@gmail.com>
2025-06-15 19:19:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    window.scrollTo({
+      top: 60, // количество пикселей
+      behavior: "smooth", // плавная прокрутка (по желанию)
+    });
+    console.log("cheker");
+  }
+);
